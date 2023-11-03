50f88dc6a9e1c8a727b4f442648be7017c5ecf3d
saurabh.nahar <saurabh.n.nahar@gmail.com>
2023-11-02 23:08:39 -0400

    create a youtube landing page and sidebar which retracts and is built using redux-toolkit

--- src/constants/videoApi.js
+++ src/constants/videoApi.js
@@ -1,5 +1,1 @@
-const apiKey = "AIzaSyC7_KwtrCV-mmw3PIck-hNS4JbvqbLr-Xk";
-
-
-export const videoApi = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key="+apiKey 
-
+export const videoApi = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`;
